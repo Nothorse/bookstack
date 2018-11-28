cbf3a3f06cf88d99dbc004289c3bf583fc9989ae
Nothorse <th@grendel.at>
2018-11-28 14:43:03 +0100

    more stuff for downloading a book

--- checkDownload.sh
+++ checkDownload.sh
@@ -3,6 +3,6 @@ DL=`which fanficfare`
 if [ -f /tmp/download_queued ]; then
     URL=`cat /tmp/download_queued`
     rm /tmp/download_queued
-    $DL "$URL" >> /tmp/ebooklib.log 2>&1 &
+    nohup $DL "$URL" >> /tmp/ebooklib.log 2>&1 &
     echo "Passed $URL to fanficfare" >> /tmp/ebooklib.log
 fi;
