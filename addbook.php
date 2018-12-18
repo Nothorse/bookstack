52525e5404ac269c0aad947edeb3e90e50ee84d1
Nothorse <nothorse.fanfic@gmail.com>
2018-12-18 11:31:55 +0100

    composerified addbook script

--- addbook.php
+++ addbook.php
@@ -1,19 +1,14 @@
 #!/usr/bin/php
 <?php
 namespace EBookLib;
-//ini_set('register_argc_argv', 'On');
-// ini_set('display_errors', '0');     # don't show any errors...
-//error_reporting(E_ALL | E_STRICT);
 define('SHELL', true);
 require_once(__DIR__ . "/config.php");
 require __DIR__ . "/vendor/autoload.php";
+
 /**
- * Needs to manually require the parent class, as it is called outside the weblication framework
+ * Command line script ot add book
+ * 
  */
-#$path = '/usr/lib/php/pear';
-
-#set_include_path(get_include_path() . PATH_SEPARATOR . $path);
-
 class AddBook extends CommandLine {
 
   /**
@@ -35,17 +30,16 @@ class AddBook extends CommandLine {
     global $argv;
     $log = __DIR__ . "/tmp/ebooklib.log";
     $file = $this->getArgument('FILE');
-    error_log("ADDBOOK Given $file\n", 3, $log);
+    $lib = new Library();
+    $lib->logThis("add $file\n");
     if($file && file_exists($file) && strpos($file, '.epub') > 0) {
-      error_log("ADDBOOK Trying to add $file\n",3, $log);
       $book = new Ebook($file);
       $book->file = $book->cleanupFile($file);
       //$growl  = "/usr/local/bin/terminal-notifier ";
       //$growl .= " -n 'Giles (Ebooklib)' ";
       $growl = str_ireplace(array("'", '"', ';'), '', $book->title) . " by " . $book->author;
-      error_log("ADDED $growl (" . $book->getFullFilePath() .")\n",3, $log);
-      $lib = new Library();
       $lib->insertBook($book);
+      $lib->logThis("Added book $growl (" . $book->getFullFilePath());
       return true;
     } else {
       echo "No ebook given.\n";
