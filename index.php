f47507d927f1b6a2f5e93c7e9ee609f67a478e09
Nothorse <nothorse.fanfic@gmail.com>
2019-01-16 14:14:14 +0100

    Update index.php
    remove last trace of ODPS

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require "vendor/autoload.php";
 //deactivate errordisplay
 //error_reporting(0);
 define('SHELL', false);
-$index = new EBookLib\Dispatcher(PORT == ODPS);
+$index = new EBookLib\Dispatcher();
 $path = (isset($_SERVER['PATH_INFO'])) ? explode('/', trim($_SERVER['PATH_INFO'], '/')) : array('');
 $time = microtime(true);
 $index->handleRequest($path);
